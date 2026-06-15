8a95c148da77c991cf9625c5772ae64b19802860
Umesh Nagar <umeshnagar9152@gmail.com>
2026-06-16 00:39:16 +0530

    CryptoTracker

--- WebScrappingProjects/CryptoPriceTracker.py
+++ WebScrappingProjects/CryptoPriceTracker.py
@@ -1,7 +1,9 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
 URL="https://api.coingecko.com/api/v3/coins/markets"
 
 PARAMS={
@@ -20,5 +22,45 @@ def get_data():
 def save_data_CSV(data):
     if not os.path.exists(filename):
         with open(filename,'w',newline='',encoding='utf-8') as f:
-            f.write("timestamp","coin","price")
+            writer=csv.writer(f)
+            writer.writerow(["timestamp","coin","price"])
+        
+    with open(filename,'a',newline='',encoding='utf-8') as f:
+        timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        writer=csv.writer(f)
+        for coin in data:
+            writer.writerow([timestamp,coin['id'],coin['current_price']])
+
+def graph_plotting(coin_name):
+    times=[]
+    prices=[]
+    with open(filename,'r',encoding='utf-8')as f:
+        reader=csv.DictReader(f)
+        for row in reader:
+            if row['coin']==coin_name:
+                times.append(row['timestamp'])
+                prices.append(float(row['price']))
+    if not times:
+        print(f"No data found for {coin_id}")
+        return
+    
+    plt.figure(figsize=(10,5))
+    plt.plot(times, prices, marker='o')
+    plt.tight_layout()
+    plt.grid()
+    plt.show()
+
+def main():
+    data=get_data()
+    save_data_CSV(data)
+    coin_name=input("Enter the coin name to plot the graph: ")
+    if coin_name:
+        graph_plotting(coin_name)
+
+if __name__=="__main__":    
+    main()
+
+
+
+
 
